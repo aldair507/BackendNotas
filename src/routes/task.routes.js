607b8430422e09159c1a8c8ad4a27395c0c2b9a9
Aldair <aldairtorres507@gmail.com>
2024-10-08 01:46:01 -0500

    cambio11

--- src/routes/task.routes.js
+++ src/routes/task.routes.js
@@ -8,8 +8,8 @@ const router = Router();
 
 router.get('/tasks',authRequired,getTasks);
 router.get('/tasks/:id',authRequired,getTask);
-router.post('/tasks',authRequired,validateSchema(createTaskSchema),createTasks);
+router.post('/tasks',validateSchema(createTaskSchema),createTasks);
 router.delete('/tasks/:id',authRequired,deleteTasks);
 router.put('/tasks/:id',authRequired,updateTasks);
 
-export default router;
+export default router;  
