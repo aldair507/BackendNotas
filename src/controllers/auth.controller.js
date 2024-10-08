91d5fc5b9a40f09a9e1821d9c5a993892beae965
Aldair <aldairtorres507@gmail.com>
2024-10-08 01:08:49 -0500

    cambio7

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -45,30 +45,30 @@ export const register = async (req, res) => {
     }
 };      
 
-export const login = async (req,res)=> {
-    const {email,password} = req.body
-    try{
+export const login = async (req, res) => {
+    const { email, password } = req.body;
+    try {
+        const userFound = await User.findOne({ email });
+        if (!userFound) return res.status(400).json({ message: "User not found" });
 
-        const userFound = await User.findOne({email});
-        if(!userFound) return res.status(400).json({message: "User not found"})
-        const isMatch = await bcrypt.compare(password,userFound.password);
+        const isMatch = await bcrypt.compare(password, userFound.password);
+        if (!isMatch) return res.status(400).json({ message: "Incorrect password" });
 
-        if(!isMatch) return res.status(400).json({message: "incorrect password"});
-
-        const token = await createAccessToken({id:userFound._id});
-        res.cookie('token',token);
+        const token = await createAccessToken({ id: userFound._id });
+        res.cookie('token', token);
         res.json({
-            id : userFound._id,
-            username : userFound.username,
-            email : userFound.email,
+            id: userFound._id,
+            username: userFound.username,
+            email: userFound.email,
             createdAt: userFound.createdAt,
             updatedAt: userFound.updatedAt,
         });
-
-    }catch(err){
-        res.status(500).json({message:err.message})
+    } catch (err) {
+        console.error('Error in login function:', err);
+        res.status(500).json({ message: 'An error occurred during login' });
     }
 };
+
 
 
 export const logout = (req, res) => {
